d55207c67a18bef86e587fa35ff597856e4bffa6
Destin Moulton <destin.moulton@gmail.com>
2019-03-21 14:13:10 -0600

    New files.

--- src/util.js
+++ src/util.js
@@ -1,3 +1,38 @@
+/**
+ * Determine if an element has a class.
+ *
+ * @param {HTMLElement} ele
+ * @param {string} cls
+ */
+function hasClass(ele, cls) {
+    return !!ele.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)"));
+}
+
+/**
+ * Add a class to an element.
+ *
+ * @param {HTMLElement} ele
+ * @param {string} cls
+ */
+function addClass(ele, cls) {
+    if (!hasClass(ele, cls)) ele.className += " " + cls;
+}
+
+/**
+ * Remove a class from an element
+ *
+ * @param {HTMLElement} ele
+ * @param {string} cls
+ */
+function removeClass(ele, cls) {
+    if (hasClass(ele, cls)) {
+        var reg = new RegExp("(\\s|^)" + cls + "(\\s|$)");
+        ele.className = ele.className.replace(reg, " ");
+    }
+}
+
 export default {
-    removeClass: (el, className) => {}
+    hasClass,
+    addClass,
+    removeClass
 };
