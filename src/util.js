9bd1d9acec7ac6dfb87e0340d061c08cfe033436
Destin Moulton <destin.moulton@gmail.com>
2019-03-21 18:28:15 -0600

    Added util methods.

--- src/util.js
+++ src/util.js
@@ -31,8 +31,61 @@ function removeClass(ele, cls) {
     }
 }
 
+function fadeIn(el, time, cb) {
+    el.style.opacity = 0;
+
+    var last = +new Date();
+    var tick = function() {
+        el.style.opacity = +el.style.opacity + (new Date() - last) / time;
+        last = +new Date();
+
+        if (+el.style.opacity < 1) {
+            (window.requestAnimationFrame && requestAnimationFrame(tick)) ||
+                setTimeout(tick, 16);
+        } else {
+            return cb();
+        }
+    };
+
+    tick();
+}
+
+/**
+ * Computer the outer height of an element.
+ *
+ * Source: https://stackoverflow.com/a/47536372
+ * @param {HTMLElement} el
+ */
+function outerHeight(el) {
+    let style = window.getComputedStyle(el);
+    return [
+        "height",
+        "padding-top",
+        "padding-bottom",
+        "margin-top",
+        "margin-bottom"
+    ]
+        .map(key => parseInt(style.getPropertyValue(key), 10))
+        .reduce((prev, cur) => prev + cur);
+}
+
+function outerWidth(el) {
+    let style = window.getComputedStyle(el);
+    return [
+        "width",
+        "padding-left",
+        "padding-right",
+        "margin-left",
+        "margin-right"
+    ]
+        .map(key => parseInt(style.getPropertyValue(key), 10))
+        .reduce((prev, cur) => prev + cur);
+}
 export default {
     hasClass,
     addClass,
-    removeClass
+    removeClass,
+    fadeIn,
+    outerHeight,
+    outerWidth
 };
